f7c8840210fbfdc38d3259757f7990cfb9feeeba
oleg_vl <oleg_vl@nitro-pc>
2023-03-04 16:18:36 +0300

    added pandas

--- sql.py
+++ sql.py
@@ -1,5 +1,16 @@
-q1 = '''SELECT Meteorites.name, Geodata.latitude, Geodata.longtitude 
+q1 = '''
+--sql
+SELECT Meteorites.name, Geodata.latitude, Geodata.longtitude 
 FROM Meteorites INNER JOIN Geodata ON (Meteorites.id = Geodata.geo_id)'''
 
-year1980 = '''SELECT Meteorites.id, Meteorites.year, Geodata.latitude, Geodata.longtitude FROM Meteorites INNER JOIN Geodata ON (Meteorites.id = Geodata.geo_id) 
+year1980 = '''
+--sql
+SELECT Meteorites.id, Meteorites.year, Geodata.latitude, Geodata.longtitude FROM Meteorites INNER JOIN Geodata ON (Meteorites.id = Geodata.geo_id) 
 WHERE Meteorites.year > 1980 and Geodata.latitude <> 0'''
+
+query  = """
+--sql
+SELECT Meteorites.name, Meteorites.nametype, Meteorites.recclass, Meteorites.mass, Meteorites.year, Geodata.place, Geodata.country, Geodata.state FROM Meteorites INNER JOIN Geodata ON (Meteorites.id = Geodata.geo_id)
+WHERE Geodata.place IS NOT NULL
+;
+"""
